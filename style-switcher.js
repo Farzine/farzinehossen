044761e2a815053d66bed458834e11a4de1cc159
shaeakh <shaeakh12@gmail.com>
2024-02-18 19:09:12 +0600

    Add style switcher and update CSS

---
+++ style-switcher.js
@@ -0,0 +1,59 @@
+/*================================== toggle style switcher ==================================*/
+// const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+// styleSwitcherToggle.addEventListener("click", () => {
+//     document.querySelector(".style-switcher").classList.toggle("open");
+// })
+// //hide style - switcher on scroll
+// window.addEventListener("scroll", () => {
+//     if (document.querySelector(".style-switcher").classList.contains("open")) {
+//         document.querySelector(".style-switcher").classList.remove("open");
+//     }
+// })
+/*================================== theme color ==================================*/
+// const alternateStyles = document.querySelectorAll(".alternate-style");
+// function setActiveStyle(color) {
+//     alternateStyles.forEach((style) => {
+//         if (color === style.getAttribute("title")) {
+//             style.removeAttribute("disabled");
+//         }
+//         else {
+//             style.setAttribute("disabled", "true");
+//         }
+//     })
+// }
+/*================================== theme light and dark mode ==================================*/
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+/*================================== Extra ==================================*/
+// const sections = document.querySelectorAll("section");
+// const navLinks = document.querySelectorAll(".nav li a");
+
+// window.addEventListener("scroll", () => {
+//     let current = "";
+//     sections.forEach((section) => {
+//         const sectionTop = section.offsetTop;
+//         const sectionHeight = section.clientHeight;
+//         if (pageYOffset >= sectionTop - sectionHeight / 3) {
+//             current = section.getAttribute("id");
+//         }
+//     });
+
+//     navLinks.forEach((link) => {
+//         link.classList.remove("active");
+//         if (link.getAttribute("href").slice(1) === current) {
+//             link.classList.add("active");
+//         }
+//     });
+// });
