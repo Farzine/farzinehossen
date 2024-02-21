617be04d66edf5d095493f193c0186df7ef50eb8
Farzine <farzine07@student.sust.edu>
2024-02-21 11:13:25 +0600

    Intregrate chat bot and more

---
+++ Home/chatbot.js
@@ -0,0 +1,18 @@
+
+window.embeddedChatbotConfig = {
+chatbotId: "oCmfub7OrCVhR4LXI0ujP",
+domain: "www.chatbase.co"
+}
+
+{/* <script>
+window.embeddedChatbotConfig = {
+chatbotId: "oCmfub7OrCVhR4LXI0ujP",
+domain: "www.chatbase.co"
+}
+</script>
+<script
+src="https://www.chatbase.co/embed.min.js"
+chatbotId="oCmfub7OrCVhR4LXI0ujP"
+domain="www.chatbase.co"
+defer>
+</script> */}
